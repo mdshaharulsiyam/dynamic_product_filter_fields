252e7ecc20a367ac8ec2e30bf8e69d6114e67fc3
Your Namecd <shaharulsiyam0273@gmail.com>
2025-08-05 12:02:58 +0600

    category modify

--- src/app/modules/category/category.interface.ts
+++ src/app/modules/category/category.interface.ts
@@ -1,7 +1,9 @@
 import { Types } from 'mongoose';
 export interface ICategory {
-    name: string;
-    category_image: string;
-    isDeleted: boolean;
-    parentCategory: Types.ObjectId | null;
+  name: string;
+  category_image: string;
+  isDeleted: boolean;
+  parentCategory: Types.ObjectId | null;
+  is_add_product: boolean;
+  is_parent_adding_product: boolean;
 }
