00a34bc296cb4bc5760f4a74d6ae7b288245b72b
manik <maniksarker652@gmail.com>
2025-08-12 10:38:22 +0600

    create api for proeduct delete

--- src/app/modules/Products/product.routes.ts
+++ src/app/modules/Products/product.routes.ts
@@ -7,27 +7,33 @@ import ProductController from './product.controller';
 const router = express.Router();
 
 router.post(
-  "/create/:fieldsReference",
-  auth(USER_ROLE.superAdmin),
-  uploadFile(),
-  // (req, res, next) => {
-  //   if (req.body.data) {
-  //     req.body = JSON.parse(req.body.data);
-  //   }
-  //   next();
-  // },
-  ProductController.Create
+    '/create/:fieldsReference',
+    auth(USER_ROLE.superAdmin),
+    uploadFile(),
+    // (req, res, next) => {
+    //   if (req.body.data) {
+    //     req.body = JSON.parse(req.body.data);
+    //   }
+    //   next();
+    // },
+    ProductController.Create
 );
 
 router.get(
-  "/get-all",
-  // auth(USER_ROLE.user),
-  ProductController.getAll
+    '/get-all',
+    // auth(USER_ROLE.user),
+    ProductController.getAll
 );
 router.get(
-  "/get-single/:id",
-  // auth(USER_ROLE.user),
-  ProductController.getSingle
+    '/get-single/:id',
+    // auth(USER_ROLE.user),
+    ProductController.getSingle
+);
+
+router.delete(
+    '/delete/:id',
+    auth(USER_ROLE.superAdmin),
+    ProductController.deleteProduct
 );
 
 export const ProductRoutes = router;
--- src/app/modules/Products/product.service.ts
+++ src/app/modules/Products/product.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { PipelineStage } from 'mongoose';
 import AppError from '../../error/appError';
 import { verifyFields, verifyObjectIds } from '../../utilities/verifyFields';
@@ -5,248 +6,265 @@ import FieldsServices from '../fields/fields.service';
 import { Product } from './product.model';
 
 const create = async (fieldsReference: string, payload: any, user: string) => {
-  try {
-    const fields = await FieldsServices.getFields(fieldsReference);
+    try {
+        const fields = await FieldsServices.getFields(fieldsReference);
 
-    const providedFields = Object.keys(payload) || [];
+        const providedFields = Object.keys(payload) || [];
 
-    const fieldNames = fields.map(field => field?.name);
-    const categoryFields = fields.filter((field: any) => field?.category?._id)?.map((field: any) => field?.name) || [];
-    const validate = await verifyFields(fieldNames, providedFields);
-    const objectIdValidation = await verifyObjectIds(categoryFields, payload);
-    if (validate.error) {
-      throw new AppError(
-        400,
-        validate.message
-      );
+        const fieldNames = fields.map((field) => field?.name);
+        const categoryFields =
+            fields
+                .filter((field: any) => field?.category?._id)
+                ?.map((field: any) => field?.name) || [];
+        const validate = await verifyFields(fieldNames, providedFields);
+        const objectIdValidation = await verifyObjectIds(
+            categoryFields,
+            payload
+        );
+        if (validate.error) {
+            throw new AppError(400, validate.message);
+        }
+        if (objectIdValidation.error) {
+            throw new AppError(400, objectIdValidation.message);
+        }
+        return await Product.create({
+            ...payload,
+            fieldsReference,
+            createdBy: user,
+            isApprove: false,
+        });
+    } catch (error: any) {
+        throw new AppError(500, error?.message || 'Internal Server Error');
     }
-    if (objectIdValidation.error) {
-      throw new AppError(
-        400,
-        objectIdValidation.message
-      );
-    }
-    return await Product.create({
-      ...payload,
-      fieldsReference,
-      createdBy: user,
-      isApprove: false
-    });
-  } catch (error: any) {
-    throw new AppError(500, error?.message || 'Internal Server Error');
-  }
 };
 
 async function GetAll(query: Record<string, any>) {
-  const page = parseInt(query.page as string) || 1;
-  const limit = parseInt(query.limit as string) || 10;
-  const skip = (page - 1) * limit;
-  const { page: pages, limit: limits, categoryFields, ...filter } = query;
-  const categoryFieldsArray: string[] = categoryFields ? categoryFields.split('_') : [];
-  const matchConditions: any = {
-    ...filter,
-  };
+    const page = parseInt(query.page as string) || 1;
+    const limit = parseInt(query.limit as string) || 10;
+    const skip = (page - 1) * limit;
+    const { page: pages, limit: limits, categoryFields, ...filter } = query;
+    const categoryFieldsArray: string[] = categoryFields
+        ? categoryFields.split('_')
+        : [];
+    const matchConditions: any = {
+        ...filter,
+    };
 
-  const lockups = categoryFieldsArray?.length > 0
-    ? categoryFieldsArray.reduce<PipelineStage[]>((acc, field) => {
-      acc.push({
-        $addFields: {
-          [field]: { $toObjectId: `$${field}` },
+    const lockups =
+        categoryFieldsArray?.length > 0
+            ? categoryFieldsArray.reduce<PipelineStage[]>((acc, field) => {
+                  acc.push({
+                      $addFields: {
+                          [field]: { $toObjectId: `$${field}` },
+                      },
+                  });
+
+                  acc.push({
+                      $lookup: {
+                          from: 'categories',
+                          localField: field,
+                          foreignField: '_id',
+                          as: field,
+                      },
+                  });
+                  acc.push({
+                      $unwind: {
+                          path: `$${field}`,
+                          preserveNullAndEmptyArrays: true,
+                      },
+                  });
+
+                  return acc;
+              }, [])
+            : [];
+    const data = await Product.aggregate([
+        {
+            $match: matchConditions,
         },
-      });
-
-      acc.push({
-        $lookup: {
-          from: 'categories',
-          localField: field,
-          foreignField: '_id',
-          as: field,
+        ...lockups,
+        {
+            $addFields: {
+                createdBy: { $toObjectId: `$createdBy` },
+            },
         },
-      });
-      acc.push({
-        $unwind: {
-          path: `$${field}`,
-          preserveNullAndEmptyArrays: true,
+        {
+            $lookup: {
+                from: 'users',
+                localField: 'createdBy',
+                foreignField: '_id',
+                as: 'createdBy',
+            },
         },
-      });
+        {
+            $unwind: {
+                path: `$createdBy`,
+                preserveNullAndEmptyArrays: true,
+            },
+        },
+        {
+            $addFields: {
+                category: { $toObjectId: `$category` },
+            },
+        },
+        {
+            $lookup: {
+                from: 'categories',
+                localField: 'category',
+                foreignField: '_id',
+                as: 'category',
+            },
+        },
+        {
+            $unwind: {
+                path: `$category`,
+                preserveNullAndEmptyArrays: true,
+            },
+        },
+        {
+            $facet: {
+                result: [{ $skip: skip }, { $limit: limit }],
+                totalCount: [{ $count: 'total' }],
+            },
+        },
+    ]);
 
-      return acc;
-    }, [])
-    : [];
-  const data = await Product.aggregate([
-    {
-      $match: matchConditions,
-    },
-    ...lockups,
-    {
-      $addFields: {
-        "createdBy": { $toObjectId: `$createdBy` },
-      }
-    },
-    {
-      $lookup: {
-        from: 'users',
-        localField: "createdBy",
-        foreignField: '_id',
-        as: 'createdBy',
-      },
-    },
-    {
-      $unwind: {
-        path: `$createdBy`,
-        preserveNullAndEmptyArrays: true,
-      },
-    },
-    {
-      $addFields: {
-        "category": { $toObjectId: `$category` },
-      }
-    },
-    {
-      $lookup: {
-        from: 'categories',
-        localField: "category",
-        foreignField: '_id',
-        as: 'category',
-      },
-    },
-    {
-      $unwind: {
-        path: `$category`,
-        preserveNullAndEmptyArrays: true,
-      },
-    },
-    {
-      $facet: {
-        result: [{ $skip: skip }, { $limit: limit }],
-        totalCount: [{ $count: 'total' }],
-      },
-    },
-  ]);
+    const result = data[0]?.result || [];
+    const total = data[0]?.totalCount[0]?.total || 0;
+    const totalPage = Math.ceil(total / limit);
 
-  const result = data[0]?.result || [];
-  const total = data[0]?.totalCount[0]?.total || 0;
-  const totalPage = Math.ceil(total / limit);
-
-  return {
-    meta: {
-      page,
-      limit,
-      total,
-      totalPage,
-    },
-    result,
-  };
+    return {
+        meta: {
+            page,
+            limit,
+            total,
+            totalPage,
+        },
+        result,
+    };
 }
 
 async function GetSingle(id: string) {
-  try {
-    const initialProduct = await Product.findById(id) as any;
-    if (!initialProduct) {
-      throw new AppError(404, 'Product not found');
+    try {
+        const initialProduct = (await Product.findById(id)) as any;
+        if (!initialProduct) {
+            throw new AppError(404, 'Product not found');
+        }
+        const fields = await FieldsServices.getFields(
+            initialProduct.fieldsReference
+        );
+
+        const categoryFields =
+            fields
+                .filter((field: any) => field?.category?._id)
+                ?.map((field: any) => field?.name) || [];
+        const lockups =
+            categoryFields?.length > 0
+                ? categoryFields.reduce<PipelineStage[]>((acc, field) => {
+                      acc.push({
+                          $addFields: {
+                              [field]: { $toObjectId: `$${field}` },
+                          },
+                      });
+
+                      acc.push({
+                          $lookup: {
+                              from: 'categories',
+                              localField: field,
+                              foreignField: '_id',
+                              as: field,
+                          },
+                      });
+                      acc.push({
+                          $unwind: {
+                              path: `$${field}`,
+                              preserveNullAndEmptyArrays: true,
+                          },
+                      });
+
+                      return acc;
+                  }, [])
+                : [];
+
+        const data = await Product.aggregate([
+            {
+                $match: {
+                    _id: { $eq: initialProduct._id },
+                },
+            },
+            ...lockups,
+            {
+                $addFields: {
+                    createdBy: { $toObjectId: `$createdBy` },
+                },
+            },
+            {
+                $lookup: {
+                    from: 'users',
+                    localField: 'createdBy',
+                    foreignField: '_id',
+                    as: 'createdBy',
+                },
+            },
+            {
+                $unwind: {
+                    path: `$createdBy`,
+                    preserveNullAndEmptyArrays: true,
+                },
+            },
+            {
+                $addFields: {
+                    category: { $toObjectId: `$category` },
+                },
+            },
+            {
+                $lookup: {
+                    from: 'categories',
+                    localField: 'category',
+                    foreignField: '_id',
+                    as: 'category',
+                },
+            },
+            {
+                $unwind: {
+                    path: `$category`,
+                    preserveNullAndEmptyArrays: true,
+                },
+            },
+            {
+                $addFields: {
+                    location: { $toObjectId: `$location` },
+                },
+            },
+            {
+                $lookup: {
+                    from: 'locations',
+                    localField: 'location',
+                    foreignField: '_id',
+                    as: 'location',
+                },
+            },
+            {
+                $unwind: {
+                    path: `$location`,
+                    preserveNullAndEmptyArrays: true,
+                },
+            },
+        ]);
+        return {
+            ...data[0],
+        };
+    } catch (error: any) {
+        throw new AppError(500, error?.message || 'Internal Server Error');
     }
-    const fields = await FieldsServices.getFields(initialProduct.fieldsReference);
-
-    const categoryFields = fields.filter((field: any) => field?.category?._id)?.map((field: any) => field?.name) || [];
-    const lockups = categoryFields?.length > 0
-      ? categoryFields.reduce<PipelineStage[]>((acc, field) => {
-        acc.push({
-          $addFields: {
-            [field]: { $toObjectId: `$${field}` },
-          },
-        });
-
-        acc.push({
-          $lookup: {
-            from: 'categories',
-            localField: field,
-            foreignField: '_id',
-            as: field,
-          },
-        });
-        acc.push({
-          $unwind: {
-            path: `$${field}`,
-            preserveNullAndEmptyArrays: true,
-          },
-        });
-
-        return acc;
-      }, [])
-      : [];
-
-    const data = await Product.aggregate([
-      {
-        $match: {
-          _id: { $eq: initialProduct._id },
-        },
-      },
-      ...lockups,
-      {
-        $addFields: {
-          "createdBy": { $toObjectId: `$createdBy` },
-        }
-      },
-      {
-        $lookup: {
-          from: 'users',
-          localField: "createdBy",
-          foreignField: '_id',
-          as: 'createdBy',
-        },
-      },
-      {
-        $unwind: {
-          path: `$createdBy`,
-          preserveNullAndEmptyArrays: true,
-        },
-      },
-      {
-        $addFields: {
-          "category": { $toObjectId: `$category` },
-        }
-      },
-      {
-        $lookup: {
-          from: 'categories',
-          localField: "category",
-          foreignField: '_id',
-          as: 'category',
-        },
-      },
-      {
-        $unwind: {
-          path: `$category`,
-          preserveNullAndEmptyArrays: true,
-        },
-      },
-      {
-        $addFields: {
-          "location": { $toObjectId: `$location` },
-        }
-      },
-      {
-        $lookup: {
-          from: 'locations',
-          localField: "location",
-          foreignField: '_id',
-          as: 'location',
-        },
-      },
-      {
-        $unwind: {
-          path: `$location`,
-          preserveNullAndEmptyArrays: true,
-        },
-      },
-    ]);
-    return {
-      ...data[0],
-    };
-  } catch (error: any) {
-    throw new AppError(500, error?.message || 'Internal Server Error');
-  }
 }
 
-const Product_Service = { create, GetAll, GetSingle };
+const deleteProduct = async (id: string) => {
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+        throw new AppError(404, 'Product not found');
+    }
+    return product;
+};
+
+const Product_Service = { create, GetAll, GetSingle, deleteProduct };
 export default Product_Service;
